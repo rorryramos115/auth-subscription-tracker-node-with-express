cdf999cee0fcee8cec5e8636d36efa09de2be900
rorry <rorryramos115@gmail.com>
2025-04-25 20:40:58 +0800

    implmement authurization access

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
+import authorize from '../middlewares/auth.middleware.js';
+import { getUser, getUsers } from '../controllers/user.contoller.js';
+
 const userRouter = Router();
 
-userRouter.get('/', (req, res) => res.send({title: "GET all user"}));
-userRouter.get('/:id', (req, res) => res.send({title: "GET user details"}));
+userRouter.get('/', getUsers);
+userRouter.get('/:id', authorize, getUser);
 userRouter.post('/', (req, res) => res.send({title: "Create new user"}));
 userRouter.put('/:id', (req, res) => res.send({title: "Update user"}));
 userRouter.delete('/:id', (req, res) => res.send({title: "Delete user"}));
