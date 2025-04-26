449679978ea8dd6a12d92c9161f1e5aedbaab165
rorry <rorryramos115@gmail.com>
2025-04-26 12:06:51 +0800

    implement upstash workflow

--- controllers/subscription.conrtoller.js
+++ controllers/subscription.conrtoller.js
@@ -1,4 +1,7 @@
 import Subscription from '../models/subscription.model.js';
+import { workflowClient } from '../config/upstash.js'
+import { SERVER_URL } from '../config/env.js'
+
 
 export const createSubscription = async (req, res, next) => {
     try {
@@ -7,9 +10,20 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id,
         });
 
+        const { workflowRunId } = await workflowClient.trigger({
+            url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+            body: {
+                subscriptionId: subscription.id,
+            },
+            headers: {
+                'content-type': 'application/json',
+            },
+            retries: 0,
+        });
+
         res.status(201).json({
             success: true,
-            data: subscription,
+            data: { subscription, workflowRunId}
         });
         
     } catch (error) {
